cd98bf3e7b259630f85460731191590a71fb912f
Dela Cruz <ldelacruz@ZHOBM-FZQ0UJL414.attlocal.net>
2020-11-03 06:55:08 -0800

    Able tp read a file

--- subc.cpp
+++ subc.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
-
+#include <fstream>
 using namespace std;
 
+void Tiny() {
+
+}
+
 int main() {
-    cout << "Hello world" << endl;
+    ifstream fin;
+    
+    fin.open("testfile.txt", ios::in);
+    if (fin.fail()) {
+        cout << "Error opening the file." << endl;
+        fin.close();
+        return -1;
+    }
+
+    char my_c;
+    int numberLines = 0;
+
+    while (!fin.eof()) {
+        fin.get(my_c);
+        cout << my_c;
+        if (my_c == '\n') {
+            ++numberLines;
+        }
+    }
+    cout << endl;
+    cout << "Number of lines: " << numberLines + 1 << endl;
+    fin.close();
     return 0;
 }

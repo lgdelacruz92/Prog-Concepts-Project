ae0b1b85b358ecaabd940e0b37237d9ed6dac0bc
lgdelacruz <lgdelacruz@lgdelacruz.com>
2020-12-05 12:26:36 -0800

    printAST

--- subc.cpp
+++ subc.cpp
@@ -4,6 +4,22 @@
 
 using namespace std;
 
+void printAST(AST* ast, int n) {
+    for (int i = 0; i < n; i++) {
+        cout << ". ";
+    }
+    cout << ast->value << endl;
+    if (ast->right) {
+        while (ast->right != nullptr) {
+            ast = ast->right;
+            n++;
+        }
+        printAST(ast, n);
+    } else if (ast->left) {
+        printAST(ast->left, n+1);
+    }
+}
+
 int main(int argc, char **argv) {
     ifstream fin;
     fin.open(argv[argc-1], ios::in);
@@ -14,6 +30,7 @@ int main(int argc, char **argv) {
     }
     Parser p(&fin);
     p.Tiny();
+    printAST(p.ast_stack.top(), 0);
     fin.close();
     return 0;
 }

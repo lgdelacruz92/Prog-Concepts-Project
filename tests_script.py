b6d7a1cc0fc4467b63c3554c9a4165c03cb38a72
lgdelacruz <lgdelacruz@lgdelacruz.com>
2020-12-15 05:27:33 -0800

    So far so good

--- tests_script.py
+++ tests_script.py
@@ -16,9 +16,9 @@ if __name__ == "__main__":
     for i in range(1, 26):
         number_suffix = '0{number}'.format(number=i) if (i < 10) else '{number}'.format(number=i)
         tiny = 'tiny_{number_suffix}'.format(number_suffix=number_suffix)
-        os.system('./subc -ast tiny_test_progs/{tiny} > tests/trees/{tiny}_result.tree 2>/dev/null'.format(tiny=tiny))
-        os.system('diff tests/trees/{tiny}_result.tree tiny_test_progs/{tiny}.tree > tests/diff/{tiny}_diff'.format(tiny=tiny))
-        if is_empty('tests/diff/{tiny}_diff'.format(tiny=tiny)):
+        os.system('./subc -ast tiny_test_progs/{tiny} > tree.{number_suffix} 2>/dev/null'.format(tiny=tiny, number_suffix=number_suffix))
+        os.system('diff tree.{number_suffix} tiny_test_progs/{tiny}.tree > {tiny}_diff'.format(number_suffix=number_suffix, tiny=tiny))
+        if is_empty('{tiny}_diff'.format(tiny=tiny)):
             print(TEXT_COLOR_GREEN + '{tiny} success!'.format(tiny=tiny) + TEXT_COLOR_END)
         else:
             print(TEXT_COLOR_RED + '{tiny} failed!'.format(tiny=tiny) + TEXT_COLOR_END)
